a003137c93e5e0ac94fb2c15c8aade1c4628107d
vladlinu <vladholik84@gmail.com>
2020-11-09 14:54:11 +0300

    changing comment
    Signed-off-by: vladlinu <vladholik84@gmail.com>

--- AndroMate/app/src/main/java/com/whiteursa/andromate/MainActivity.java
+++ AndroMate/app/src/main/java/com/whiteursa/andromate/MainActivity.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    // creating activity
+    // creating main activity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
