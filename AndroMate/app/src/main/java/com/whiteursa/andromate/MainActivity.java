955c7ccd5a2e2a678be069a69762c77693350477
vladlinu <vladholik84@gmail.com>
2020-11-28 19:36:05 +0300

    Finished with mainActivity (shows current weather)
    Signed-off-by: vladlinu <vladholik84@gmail.com>

--- AndroMate/app/src/main/java/com/whiteursa/andromate/MainActivity.java
+++ AndroMate/app/src/main/java/com/whiteursa/andromate/MainActivity.java
@@ -1,34 +1,199 @@
 package com.whiteursa.andromate;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.res.ResourcesCompat;
 
+import android.content.pm.PackageManager;
+import android.graphics.Typeface;
+import android.location.Location;
+import android.os.Build;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
+import android.os.StrictMode;
+import android.text.Html;
+import android.util.Log;
+import android.view.WindowManager;
+import android.widget.TextView;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
 
 public class MainActivity extends AppCompatActivity {
 
-    // creating main activity
+    private static final String OPEN_WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather?lat=%s&lon=%s&units=metric";
+    private static final String OPEN_WEATHER_API = "1f0baa768f962c90a6346c6372678dce";
+
+    TextView city, details, currentTemperature, humidity, pressure, weatherIcon, lastUpdated;
+    Typeface weatherFont;
+
+    static String latitude;
+    static String longtitude;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu, menu);
-        return true;
-    }
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
 
-        switch (item.getItemId()) {
-            case R.id.changeCity:
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
+
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
+        requestPermissions();
+
+        FusedLocationProviderClient mFusedLocationProviderClient;
+        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+
+        if (ActivityCompat.checkSelfPermission(MainActivity.this, ACCESS_FINE_LOCATION) !=
+                PackageManager.PERMISSION_GRANTED) {
+            return;
         }
+
+        mFusedLocationProviderClient.getLastLocation().addOnSuccessListener(MainActivity.this, new OnSuccessListener<Location>() {
+            @RequiresApi(api = Build.VERSION_CODES.O)
+            @Override
+            public void onSuccess(Location location) {
+                if (location != null) {
+                    latitude = String.valueOf(location.getLatitude());
+                    longtitude = String.valueOf(location.getLongitude());
+
+                    weatherFont = Typeface.createFromAsset(getAssets(), "font/webfont.ttf");
+
+                    city = findViewById(R.id.city);
+                    currentTemperature = findViewById(R.id.currentTemperature);
+                    lastUpdated = findViewById(R.id.lastUpdated);
+                    humidity = findViewById(R.id.humidity);
+                    pressure = findViewById(R.id.pressure);
+                    details = findViewById(R.id.details);
+                    weatherIcon = findViewById(R.id.weatherIcon);
+                    weatherIcon.setTypeface(weatherFont);
+
+                    String[] jsonData = getJSONResponse();
+
+                    city.setText(jsonData[0]);
+                    details.setText(jsonData[1]);
+                    currentTemperature.setText(jsonData[2]);
+                    humidity.setText("Humidity : " + jsonData[3]);
+                    pressure.setText("Pressure : " + jsonData[4]);
+                    lastUpdated.setText(jsonData[5]);
+                    weatherIcon.setText(Html.fromHtml(jsonData[6]));
+                }
+            }
+        });
+    }
+
+    public String[] getJSONResponse() {
+        String[] jsonData = new String[7];
+        JSONObject jsonWeather = null;
+        try {
+            jsonWeather = getWeatherJson(latitude, longtitude);
+        } catch (Exception e) {
+            Log.d("Error", "Cannot get JSON results", e);
+        }
+
+        try {
+            if (jsonWeather != null) {
+                JSONObject details = jsonWeather.getJSONArray("weather").getJSONObject(0);
+                JSONObject main = jsonWeather.getJSONObject("main");
+                DateFormat df = DateFormat.getDateInstance();
+
+                String city = jsonWeather.getString("name") + ", " +
+                        jsonWeather.getJSONObject("sys").getString("country");
+                String description = details.getString("description").toLowerCase(Locale.US);
+                String temperature = String.format("%.0f", main.getDouble("temp")) + "°";
+                String humidity = main.getString("humidity") + "%";
+                String pressure = main.getString("pressure") + " hPa";
+                String lastUpdated = df.format(new Date(jsonWeather.getLong("dt")*1000));
+                String icon = setWeatherIcon(details.getInt("id"), jsonWeather.getJSONObject("sys").getLong("sunrise")*1000,
+                        jsonWeather.getJSONObject("sys").getLong("sunset")*1000);
+
+                jsonData[0] = city;
+                jsonData[1] = description;
+                jsonData[2] = temperature;
+                jsonData[3] = humidity;
+                jsonData[4] = pressure;
+                jsonData[5] = lastUpdated;
+                jsonData[6] = icon;
+            }
+        } catch (Exception ex) {
+
+        }
+        return jsonData;
+    }
+
+    public static String setWeatherIcon(int actualId, long sunrise, long sunset) {
+        int id = actualId/100;
+        String icon = "";
+        if (actualId == 800) {
+            long currentTime = new Date().getTime();
+            if(currentTime >= sunrise && currentTime < sunset) {
+                icon = "&#xf00d;";
+            } else {
+                icon = "&#xf02e;";
+            }
+        } else {
+            switch (id) {
+                case 2:
+                    icon = "&#xf01e;";
+                    break;
+                case 3:
+                    icon = "&#xf01c;";
+                    break;
+                case 7:
+                    icon = "&#xf014;";
+                    break;
+                case 8:
+                    icon = "&#xf013;";
+                    break;
+                case 6:
+                    icon = "&#xf01b;";
+                    break;
+                case 5:
+                    icon = "&#xf019;";
+                    break;
+            }
+        }
+        return icon;
+    }
+
+    public static JSONObject getWeatherJson(String latitude, String longtitude) {
+        try {
+            URL url = new URL(String.format(OPEN_WEATHER_URL, latitude, longtitude));
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.addRequestProperty("x-api-key", OPEN_WEATHER_API);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            StringBuffer json = new StringBuffer(1024);
+            String tmp = "";
+            while ((tmp = reader.readLine()) != null) {
+                json.append(tmp).append("\n");
+            }
+            reader.close();
+            JSONObject data = new JSONObject(json.toString());
+            if (data.getInt("cod") != 200) {
+                return null;
+            }
+
+            return data;
+        } catch (Exception ex) {
+            return null;
+        }
+    }
+    private void requestPermissions() {
+        ActivityCompat.requestPermissions(this, new String[]{ACCESS_FINE_LOCATION}, 1);
     }
 }
