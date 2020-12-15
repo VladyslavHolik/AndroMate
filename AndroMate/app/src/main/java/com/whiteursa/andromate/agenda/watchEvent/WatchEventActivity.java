5c21bf20004f0cc62ecbb2835154482de9179b35
vladlinu <vladholik84@gmail.com>
2020-12-15 17:53:24 +0300

    Added WatchEvent activity
    Signed-off-by: vladlinu <vladholik84@gmail.com>

---
+++ AndroMate/app/src/main/java/com/whiteursa/andromate/agenda/watchEvent/WatchEventActivity.java
@@ -0,0 +1,16 @@
+package com.whiteursa.andromate.agenda.watchEvent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.whiteursa.andromate.R;
+
+public class WatchEventActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_watch_event);
+    }
+}
