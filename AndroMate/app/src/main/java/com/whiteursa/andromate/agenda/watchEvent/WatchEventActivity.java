0913f136765fd7ee58132224a2a9b655cb473b7f
vladlinu <vladholik84@gmail.com>
2020-12-15 17:57:47 +0300

    Added onClick method for watchEventActivity
    Signed-off-by: vladlinu <vladholik84@gmail.com>

--- AndroMate/app/src/main/java/com/whiteursa/andromate/agenda/watchEvent/WatchEventActivity.java
+++ AndroMate/app/src/main/java/com/whiteursa/andromate/agenda/watchEvent/WatchEventActivity.java
@@ -2,9 +2,13 @@ package com.whiteursa.andromate.agenda.watchEvent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.whiteursa.andromate.R;
+import com.whiteursa.andromate.agenda.AgendaActivity;
+import com.whiteursa.andromate.agenda.addEvent.AddEventActivity;
 
 public class WatchEventActivity extends AppCompatActivity {
 
@@ -12,5 +16,25 @@ public class WatchEventActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_watch_event);
+
+
+    }
+
+    public void onClick(View view) {
+        Intent intent = new Intent(WatchEventActivity.this,
+                AgendaActivity.class);
+        setIntentProperties(intent);
+        startActivity(intent);
+        overridePendingTransition(R.anim.right_to_center, R.anim.center_to_left);
+    }
+
+    private void setIntentProperties(Intent intent) {
+        intent.putExtra("city", getIntent().getStringExtra("city"));
+        intent.putExtra("details", getIntent().getStringExtra("details"));
+        intent.putExtra("currentTemperature", getIntent().getStringExtra("currentTemperature"));
+        intent.putExtra("humidity", getIntent().getStringExtra("humidity"));
+        intent.putExtra("pressure", getIntent().getStringExtra("pressure"));
+        intent.putExtra("lastUpdated", getIntent().getStringExtra("lastUpdated"));
+        intent.putExtra("weatherIcon",getIntent().getStringExtra("weatherIcon"));
     }
 }
