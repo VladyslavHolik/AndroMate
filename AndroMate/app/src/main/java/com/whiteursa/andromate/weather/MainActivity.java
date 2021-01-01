cfae9fc324168efcbd0365a0a6d21204d12f5792
vladlinu <vladholik84@gmail.com>
2021-01-02 00:10:16 +0300

    Redid mainActivity (Weather activity)
    Signed-off-by: vladlinu <vladholik84@gmail.com>

--- AndroMate/app/src/main/java/com/whiteursa/andromate/weather/MainActivity.java
+++ AndroMate/app/src/main/java/com/whiteursa/andromate/weather/MainActivity.java
@@ -33,11 +33,15 @@ import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
-    TextView city, details, currentTemperature, humidity, pressure, weatherIcon, lastUpdated;
-    Typeface weatherFont;
     String[] languages = {"af", "ar", "bg", "bn", "ca","cn", "cs","cy", "da", "de", "el", "en",
             "es", "et", "fa", "fi", "fr", "gu", "he", "hi", "hr", "hu", "id", "it", "ja", "kn",
             "ko", "lt", "lv", "mk", "ml", "mr", "ne", "nl", "no", "pa", "pl", "pt", "ro", "ru",
@@ -74,25 +78,116 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void setViews() {
-        weatherFont = Typeface.createFromAsset(getAssets(), "font/webfont.ttf");
-
-        city = findViewById(R.id.city);
-        currentTemperature = findViewById(R.id.currentTemperature);
-        lastUpdated = findViewById(R.id.lastUpdated);
-        humidity = findViewById(R.id.humidity);
-        pressure = findViewById(R.id.pressure);
-        details = findViewById(R.id.details);
-        weatherIcon = findViewById(R.id.weatherIcon);
-        weatherIcon.setTypeface(weatherFont);
-
-        city.setText(getIntent().getStringExtra("city"));
-        details.setText(getIntent().getStringExtra("details"));
-        currentTemperature.setText(String.format("%s°",getIntent().getStringExtra("currentTemperature")));
-        humidity.setText(getIntent().getStringExtra("humidity"));
-        pressure.setText(getIntent().getStringExtra("pressure"));
-        lastUpdated.setText(getIntent().getStringExtra("lastUpdated"));
-        weatherIcon.setText(Html.fromHtml(getIntent().getStringExtra("weatherIcon")));
+        setFonts();
+        setTexts();
     }
+
+    private void setFonts() {
+        Typeface weatherFont = Typeface.createFromAsset(getAssets(), "font/webfont.ttf");
+        Typeface cityFont = Typeface.createFromAsset(getAssets(), "font/Poppins-Medium.ttf");
+        Typeface updateFont = Typeface.createFromAsset(getAssets(), "font/Poppins-Regular.ttf");
+        Typeface weekdaysFont = Typeface.createFromAsset(getAssets(), "font/Montserrat-Medium.ttf");
+        Typeface temperatureFont = Typeface.createFromAsset(getAssets(), "font/Montserrat-Bold.ttf");
+
+        TextView city = findViewById(R.id.city);
+        TextView currentTemperature = findViewById(R.id.currentTemperature);
+        TextView lastUpdated = findViewById(R.id.lastUpdated);
+        TextView humidity = findViewById(R.id.humidity);
+        TextView windSpeed = findViewById(R.id.windSpeed);
+        TextView weatherIcon = findViewById(R.id.weatherIcon);
+
+        TextView nowWeekday = findViewById(R.id.nowWeekday);
+        TextView tomorrowWeekday = findViewById(R.id.tomorrowWeekday);
+        TextView afterTomorrowWeekday = findViewById(R.id.afterTomorrowWeekday);
+        TextView aaTomorrowWeekday = findViewById(R.id.aaTomorrowWeekday);
+        TextView aaaTomorrowWeekday = findViewById(R.id.aaaTomorrowWeekday);
+
+        TextView nowTemperature = findViewById(R.id.nowTemperature);
+        TextView tomorrowTemperature = findViewById(R.id.tomorrowTemperature);
+        TextView afterTomorrowTemperature = findViewById(R.id.afterTomorrowTemperature);
+        TextView aaTomorrowTemperature = findViewById(R.id.aaTomorrowTemperature);
+        TextView aaaTomorrowTemperature = findViewById(R.id.aaaTomorrowTemperature);
+
+        city.setTypeface(cityFont);
+        lastUpdated.setTypeface(updateFont);
+        currentTemperature.setTypeface(temperatureFont);
+        windSpeed.setTypeface(weekdaysFont);
+        humidity.setTypeface(weekdaysFont);
+
+        nowWeekday.setTypeface(weekdaysFont);
+        tomorrowWeekday.setTypeface(weekdaysFont);
+        afterTomorrowWeekday.setTypeface(weekdaysFont);
+        aaTomorrowWeekday.setTypeface(weekdaysFont);
+        aaaTomorrowWeekday.setTypeface(weekdaysFont);
+
+        nowTemperature.setTypeface(weekdaysFont);
+        tomorrowTemperature.setTypeface(weekdaysFont);
+        afterTomorrowTemperature.setTypeface(weekdaysFont);
+        aaTomorrowTemperature.setTypeface(weekdaysFont);
+        aaaTomorrowTemperature.setTypeface(weekdaysFont);
+
+        weatherIcon.setTypeface(weatherFont);
+    }
+
+    private void setTexts() {
+        TextView city = findViewById(R.id.city);
+        TextView currentTemperature = findViewById(R.id.currentTemperature);
+        TextView lastUpdated = findViewById(R.id.lastUpdated);
+        TextView humidity = findViewById(R.id.humidity);
+        TextView windSpeed = findViewById(R.id.windSpeed);
+        TextView weatherIcon = findViewById(R.id.weatherIcon);
+
+        TextView nowWeekday = findViewById(R.id.nowWeekday);
+        TextView tomorrowWeekday = findViewById(R.id.tomorrowWeekday);
+        TextView afterTomorrowWeekday = findViewById(R.id.afterTomorrowWeekday);
+        TextView aaTomorrowWeekday = findViewById(R.id.aaTomorrowWeekday);
+        TextView aaaTomorrowWeekday = findViewById(R.id.aaaTomorrowWeekday);
+
+        TextView nowTemperature = findViewById(R.id.nowTemperature);
+        TextView tomorrowTemperature = findViewById(R.id.tomorrowTemperature);
+        TextView afterTomorrowTemperature = findViewById(R.id.afterTomorrowTemperature);
+        TextView aaTomorrowTemperature = findViewById(R.id.aaTomorrowTemperature);
+        TextView aaaTomorrowTemperature = findViewById(R.id.aaaTomorrowTemperature);
+
+        ArrayList<ArrayList<String>> data = (ArrayList<ArrayList<String>>) getIntent().getSerializableExtra("data");
+        assert data != null;
+        city.setText(String.format("%s, %s", data.get(0).get(0), data.get(0).get(1)));
+
+        SimpleDateFormat formatter = new SimpleDateFormat("EEEE dd, MMMM", Locale.US);
+        Date dateNow = new Date();
+        String date = formatter.format(dateNow);
+        lastUpdated.setText(date);
+
+        currentTemperature.setText(String.format("%s°", data.get(1).get(0)));
+        weatherIcon.setText(Html.fromHtml(data.get(1).get(3)));
+        humidity.setText(String.format("Humidity\n%s%%", data.get(1).get(1)));
+        windSpeed.setText(String.format("Wind\n%s m/s", data.get(1).get(2)));
+        nowTemperature.setText(String.format("%s°", data.get(1).get(0)));
+        tomorrowTemperature.setText(String.format("%s°", data.get(2).get(0)));
+        afterTomorrowTemperature.setText(String.format("%s°", data.get(3).get(0)));
+        aaTomorrowTemperature.setText(String.format("%s°", data.get(4).get(0)));
+        aaaTomorrowTemperature.setText(String.format("%s°", data.get(5).get(0)));
+
+        formatter = new SimpleDateFormat("EEEE", Locale.US);
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(dateNow);
+        String now = formatter.format(calendar.getTime());
+        calendar.add(Calendar.DATE, 1);
+        String tomorrow = formatter.format(calendar.getTime());
+        calendar.add(Calendar.DATE, 1);
+        String afterTomorrow = formatter.format(calendar.getTime());
+        calendar.add(Calendar.DATE, 1);
+        String aaTomorrow = formatter.format(calendar.getTime());
+        calendar.add(Calendar.DATE, 1);
+        String aaaTomorrow = formatter.format(calendar.getTime());
+
+        nowWeekday.setText(now);
+        tomorrowWeekday.setText(tomorrow);
+        afterTomorrowWeekday.setText(afterTomorrow);
+        aaTomorrowWeekday.setText(aaTomorrow);
+        aaaTomorrowWeekday.setText(aaaTomorrow);
+    }
+
     public void showSettings(View view) {
         // inflate the layout of the popup window
         LayoutInflater inflater = (LayoutInflater)
