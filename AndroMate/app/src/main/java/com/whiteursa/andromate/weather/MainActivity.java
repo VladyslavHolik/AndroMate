be24878d3a59bbf391ed58de79693ed07f7f54f7
vladlinu <vladholik84@gmail.com>
2021-01-02 15:08:03 +0300

    Fixed bug with setting by replacing it with fragment
    Signed-off-by: vladlinu <vladholik84@gmail.com>

--- AndroMate/app/src/main/java/com/whiteursa/andromate/weather/MainActivity.java
+++ AndroMate/app/src/main/java/com/whiteursa/andromate/weather/MainActivity.java
@@ -1,38 +1,23 @@
 package com.whiteursa.andromate.weather;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.graphics.PorterDuff;
 import android.graphics.Typeface;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.os.StrictMode;
-import android.preference.PreferenceManager;
 import android.text.Html;
-import android.view.Gravity;
-import android.view.LayoutInflater;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.WindowManager;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.LinearLayout;
-import android.widget.PopupWindow;
-import android.widget.RadioButton;
-import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.whiteursa.andromate.R;
 import com.whiteursa.andromate.agenda.AgendaActivity;
 import com.whiteursa.andromate.news.NewsActivity;
+import com.whiteursa.andromate.settingsFragment.SettingsFragment;
 
-import java.io.UnsupportedEncodingException;
-import java.net.URLDecoder;
-import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -41,10 +26,7 @@ import java.util.Locale;
 import java.util.Objects;
 
 public final class MainActivity extends AppCompatActivity {
-    String[] languages = {"af", "ar", "bg", "bn", "ca","cn", "cs","cy", "da", "de", "el", "en",
-            "es", "et", "fa", "fi", "fr", "gu", "he", "hi", "hr", "hu", "id", "it", "ja", "kn",
-            "ko", "lt", "lv", "mk", "ml", "mr", "ne", "nl", "no", "pa", "pl", "pt", "ro", "ru",
-            "sk", "sl", "so", "sq", "sv", "sw", "ta", "te", "th", "tl", "tr","tw", "uk", "ur","vi"};
+    private boolean isFragmentDisplayed = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -217,105 +199,31 @@ public final class MainActivity extends AppCompatActivity {
     }
 
     public void showSettings(View view) {
-        // inflate the layout of the popup window
-        LayoutInflater inflater = (LayoutInflater)
-                getSystemService(LAYOUT_INFLATER_SERVICE);
-        assert inflater != null;
-        final View popupView = inflater.inflate(R.layout.settings_popup, null);
-
-        // create the popup window
-        int width = LinearLayout.LayoutParams.MATCH_PARENT;
-        int height = LinearLayout.LayoutParams.WRAP_CONTENT;
-        final PopupWindow popupWindow = new PopupWindow(popupView, width, height, true);
-
-        Button save = popupView.findViewById(R.id.saveButton);
-
-        View.OnClickListener saveListener = new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                SharedPreferences myPreferences = PreferenceManager.getDefaultSharedPreferences(MainActivity.this);
-                SharedPreferences.Editor myEditor = myPreferences.edit();
-                RadioButton male = popupView.findViewById(R.id.radioMan);
-
-                if (male.isChecked()) {
-                    myEditor.putString("GENDER", "Male");
-                } else {
-                    myEditor.putString("GENDER", "Female");
-                }
-
-                Spinner spinner = popupView.findViewById(R.id.languagesSpinner);
-                myEditor.putInt("languagePosition", spinner.getSelectedItemPosition());
-                myEditor.putString("language", languages[spinner.getSelectedItemPosition()]);
-
-                TextView textView = popupView.findViewById(R.id.searchText);
-                String textString = textView.getText().toString();
-
-                if (!textString.equals("")) {
-                    try {
-                        myEditor.putString("searchText", URLEncoder.encode(textString, StandardCharsets.UTF_8.toString()));
-                    } catch (UnsupportedEncodingException e) {
-                        e.printStackTrace();
-                    }
-                } else {
-                    textView.setError("Must not be null");
-                    return;
-                }
-                myEditor.apply();
-                popupWindow.dismiss();
-            }
-        };
-
-        save.setOnClickListener(saveListener);
-
-        SharedPreferences myPreferences = PreferenceManager.getDefaultSharedPreferences(MainActivity.this);
-        String gender = myPreferences.getString("GENDER", "Unknown");
-        if (!gender.equals("Unknown") ) {
-            if (gender.equals("Male") ) {
-                RadioButton male = popupView.findViewById(R.id.radioMan);
-                male.setChecked(true);
-            } else {
-                RadioButton female = popupView.findViewById(R.id.radioWoman);
-                female.setChecked(true);
-            }
+        if (!isFragmentDisplayed) {
+            displayFragment();
+        } else {
+            closeFragment();
         }
+    }
 
-        Spinner languagesSpinner = popupView.findViewById(R.id.languagesSpinner);
+    private void displayFragment() {
+        SettingsFragment fragment = SettingsFragment.newInstance();
+        fragment.setActivity(this);
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
 
-        Drawable spinnerDrawable = Objects.requireNonNull(languagesSpinner.getBackground().getConstantState()).newDrawable();
-        spinnerDrawable.setColorFilter(getResources().getColor(R.color.white), PorterDuff.Mode.SRC_ATOP);
+        fragmentTransaction.add(R.id.fragment_containter, fragment).addToBackStack(null).commit();
+        isFragmentDisplayed = true;
+    }
 
-        languagesSpinner.setBackground(spinnerDrawable);
-
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.spinner_item, languages);
-        adapter.setDropDownViewResource(R.layout.spinner_dropdown_item);
-        languagesSpinner.setAdapter(adapter);
-
-        int languagePosition = myPreferences.getInt("languagePosition", -1);
-        if (languagePosition != -1) {
-            languagesSpinner.setSelection(languagePosition);
+     public void closeFragment() {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        SettingsFragment fragment = (SettingsFragment) fragmentManager.findFragmentById(R.id.fragment_containter);
+        if (fragment != null) {
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.remove(fragment).commit();
         }
-
-        String searchText = myPreferences.getString("searchText", "");
-        if (!searchText.equals("")) {
-            TextView textView = popupView.findViewById(R.id.searchText);
-            try {
-                textView.setText(URLDecoder.decode(searchText, StandardCharsets.UTF_8.toString()));
-            } catch (UnsupportedEncodingException e) {
-                e.printStackTrace();
-            }
-        }
-        popupWindow.showAtLocation(view, Gravity.BOTTOM, 0, 0);
-
-        popupView.setOnTouchListener(new View.OnTouchListener() {
-            @SuppressLint("ClickableViewAccessibility")
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                popupWindow.dismiss();
-                return true;
-            }
-        });
-
-
+        isFragmentDisplayed = false;
     }
 
     private void setIntentProperties(Intent intent) {
